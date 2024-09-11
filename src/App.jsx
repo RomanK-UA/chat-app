c4e2e408db58cef03d58ad7533a41da82ef0eb90
RomanK-UA <romakartamyshev01@gmail.com>
2024-09-11 17:07:24 +0300

    Menu in progress

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import './style.css'
 import ChatDetails from './components/ChatDetails'
 import Chat from './components/Chat'
 import ChatLists from './components/ChatsList'
+import Menu from './components/Menu'
+import { useState } from 'react'
+
 function App() {
-
-
+  
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+      setIsOpen(!isOpen);
+  }
+  
   return (
     <div className="bg-[url('../src/assets/bg.jpg')] bg-cover bg-center h-screen py-5">
-      <div className="container mx-auto h-full flex items-center justify-center flex-1 p-2 border-2 rounded-xl backdrop-blur-md backdrop-saturate-150">
+      <div className="container mx-auto h-full flex items-center justify-center flex-1 p-2 border-2 rounded-xl backdrop-blur-md backdrop-saturate-150 ">
+        <Menu isOpen = {isOpen} toggleMenu = {toggleMenu} />
         <ChatLists />
         <Chat />
         <ChatDetails/>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,10 @@
 
 
-const Menu = () => {
+const Menu = ({isOpen, toggleMenu}) => {
 
     return(
-        <section className="h-full flex-1 border-2 border-red-600 p-2">
+        <section className={`${isOpen? 'block' : 'hidden'} h-full flex-1 border-2 border-red-600 p-2`}>
+            <button onClick={toggleMenu} className="text-3xl">&#x2630</button>
             Menu
         </section>
     )
