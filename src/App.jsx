36a5a30045bc482ab6e0bef9035aeceba4201951
RomanK-UA <romakartamyshev01@gmail.com>
2024-10-05 11:57:44 +0300

    side menu added

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,31 @@ import { useState } from 'react'
 import { IoIosArrowBack } from "react-icons/io";
 function App() {
   const [isOpen, setIsOpen] = useState(false);
-  function toggleMenu() {
-      setIsOpen(!isOpen);
-  }
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div className="bg-[url('../src/assets/bg.jpg')] bg-cover bg-center h-screen ">
-      <div className="relative mx-auto h-full flex items-center justify-center flex-1 p-2 border-2 rounded-xl backdrop-blur-md backdrop-saturate-150 ">
+      <div className="relative mx-auto h-full flex items-center justify-center flex-1 p-2 rounded-xl backdrop-blur-md backdrop-saturate-150  ">
+        {/* Menu Toggle Button */}
         <button onClick={toggleMenu} className='p-2 text-2xl self-start z-20'>
           {!isOpen ? <IoMdMenu /> : <IoIosArrowBack />}
         </button>
+
+        {/* Hidden Menu */}
         <Menu isOpen={isOpen} />
-        <ChatsList />
-        <Chat />
-        <ChatDetails/>
+        {isOpen && <div className="fixed inset-0 bg-black bg-opacity-50 z-10" onClick={toggleMenu} />}
+        {/* Main content */}
+        <div className="flex w-full h-full">
+          <ChatsList />
+          <Chat />
+          <ChatDetails />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,21 @@
 import UserInfo from "./UserInfo"
-const Menu = ({isOpen}) => {
-
-    return(
-            <div className={`${isOpen? 'block' : 'hidden'} h-full border-2 border-red-600 p-4 absolute left-0 top-0 w-80`}>
-            <UserInfo />
-            HIIII
-            </div>
-    )
-}
+function Menu({ isOpen }) {
+    return (
+      <div
+        className={`fixed top-0 left-0 w-96 h-full bg-slate-600 shadow-lg transform transition-transform duration-300 ease-in-out z-30 shadow-lg shadow-gray-500/50  ${
+          isOpen ? 'translate-x-0' : '-translate-x-full'
+        }`}
+      >
+        <div className="p-4">
+          <h2 className="text-lg font-bold mb-4">User Info</h2>
+          <ul>
+            <li className="mb-2">Settings</li>
+            <li className="mb-2">Profile</li>
+            <li className="mb-2">Logout</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 
 export default Menu
