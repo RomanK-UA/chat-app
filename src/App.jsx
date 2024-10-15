b94d58a0934a3c521ba15207e1eaa6cb3f495c20
RomanK-UA <romakartamyshev01@gmail.com>
2024-10-15 17:11:21 +0300

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,44 @@
-
-import './style.css'
-import ChatDetails from './components/ChatDetails'
-import Chat from './components/Chat'
-import ChatsList from './components/ChatsList'
-import Menu from './components/Menu'
+import "./style.css";
+import ChatDetails from "./components/ChatDetails";
+import Chat from "./components/Chat";
+import ChatsList from "./components/ChatsList";
+import Menu from "./components/Menu";
+import LoginForm from "./components/LoginForm";
 import { IoMdMenu } from "react-icons/io";
-import { useState } from 'react'
+import { useState } from "react";
 function App() {
   const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+  const [user, setUser] = useState({name: "name"});
+  const toggleMenu = () => setIsOpen(prev => !prev);
 
   return (
-    <div className="bg-[url('../src/assets/bg.jpg')] bg-cover bg-center h-screen ">
-      <div className="relative mx-auto h-full flex items-center justify-center flex-1 p-2 rounded-xl backdrop-blur-md backdrop-saturate-150  ">
-        {/* Menu Toggle Button */}
-        <button onClick={toggleMenu} className='p-2 text-2xl self-start z-20'> 
-          <IoMdMenu /> 
-        </button>
-        {/* Hidden Menu */}
-        <Menu isOpen={isOpen} />
-        {isOpen && <div className="fixed inset-0 bg-black bg-opacity-50 z-10" onClick={toggleMenu} />}
-        {/* Main content */}
-        <div className="flex w-full h-full">
-          <ChatsList />
-          <Chat />
-          <ChatDetails />
+    <>
+      {user ? (
+        <div className="bg-[url('../src/assets/bg.jpg')] bg-cover bg-center h-screen">
+          <div className="relative mx-auto h-full flex items-center justify-center flex-1 p-2 rounded-xl backdrop-blur-md backdrop-saturate-150">
+            <button onClick={toggleMenu} className="p-2 text-2xl self-start z-20">
+              <IoMdMenu />
+            </button>
+
+            {isOpen && (
+              <>
+                <Menu isOpen={isOpen} />
+                <div className="fixed inset-0 bg-black bg-opacity-50 z-10" onClick={toggleMenu} />
+              </>
+            )}
+
+            <div className="flex w-full h-full">
+              <ChatsList />
+              <Chat />
+              <ChatDetails />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <LoginForm />
+      )}
+    </>
   );
 }
 
-export default App
+export default App;
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,10 @@
+
+function LoginForm() {
+    return (
+        <div className="w-full h-screen bg-slate-600 content-center text-center">
+            Login
+        </div>
+    );
+}
+
+export default LoginForm;
